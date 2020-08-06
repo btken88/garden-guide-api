4954549701bbda76800abf30ca23194d5ddc0bcc
Bryce Kennedy <btken88@gmail.com>
2020-08-06 16:57:25 -0600

    Remove plants post route - didn't get saved in last commit.

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const Plant = require('./models/Plant')
 app.use(cors())
 app.use(bodyParser.json())
 
-app.post('/plants', async (req, res) => {
-  const newPlant = await Plant.query().insert(req.body)
-  res.status(201).json(newPlant)
-})
-
 app.get('/plants', (req, res) => {
   Plant.query().select('*')
     .then(data => res.json(data))
