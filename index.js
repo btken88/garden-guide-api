f50c71cccca12c8f55de9bc7580e2d6df81ac2fa
Bryce Kennedy <btken88@gmail.com>
2020-08-10 16:08:21 -0600

    Connect to heroku

--- index.js
+++ index.js
@@ -12,16 +12,25 @@ app.use(bodyParser.json())
 app.get('/plants', (req, res) => {
   Plant.query().select('*')
     .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }))
 })
 
 app.get('/plants/:id', (req, res) => {
   Plant.query().select('*').where({ id: req.params.id })
     .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }))
+})
+
+app.get('/varieties/:id', (req, res) => {
+  Variety.query().select('*').where({ id: req.params.id })
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }))
 })
 
 app.get('/varieties/plantId/:id', (req, res) => {
   Variety.query().select('*').where({ plantId: req.params.id })
     .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: err.message }))
 })
 
 app.listen(5000)
