e65e9b0d5ffa3d0cae30303335630431fed849a5
Bryce Kennedy <btken88@gmail.com>
2020-08-10 16:35:46 -0600

    Add additional postgres setup to heroku database.

--- knexfile.js
+++ knexfile.js
@@ -6,5 +6,11 @@ module.exports = {
     client: 'pg',
     useNullAsDefault: true,
     connection: 'postgres:///gardenguideapp'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: __dirname + '/db/migrations' },
+    seeds: { directory: __dirname + '/db/seeds/productions' }
   }
 };
