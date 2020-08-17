51bfc12fa7a58bba4e9317573bb6f1e99717b895
Bryce Kennedy <btken88@gmail.com>
2020-08-17 16:40:38 -0600

    Add /weather post route for proxy request to fetch user lat/long based on zip code from one API chained to fetch weather using lat/long

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const knex = require('knex')
 const morgan = require('morgan')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const fetch = require('node-fetch')
 const Plant = require('./models/Plant')
 const Variety = require('./models/Variety')
 const Todo = require('./models/Todo')
@@ -178,6 +179,14 @@ app.listen(process.env.PORT, () => {
   console.log(`Running on port ${process.env.PORT}`)
 })
 
+app.post('/weather', authorizeUser, (req, res) => {
+  User.query().select('zip').where({ id: req.body.userId })
+    .then(data => fetchLatLon(data[0].zip))
+    .then(fetchWeather)
+    .then(data => res.status(200).json(data))
+    .catch(err => res.json({ error: err.message }).status(500))
+})
+
 function authorizeUser(req, res, next) {
   const token = req.headers.authorization
   if (!token) return res.json({ error: 'Authentication failed' }).status(401)
@@ -189,4 +198,21 @@ function authorizeUser(req, res, next) {
       next()
     }
   })
+}
+
+function fetchLatLon(zip) {
+  return fetch(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${zip}`)
+    .then(response => response.json())
+    .then(result => {
+      return result.records[0].fields.geopoint
+    })
+    .catch(err => err.message)
+}
+
+
+function fetchWeather(latLon) {
+  const [lat, lon] = latLon
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${process.env.WEATHER_API_KEY}`
+  return fetch(weatherAPI)
+    .then(response => response.json())
 }
